7e4b645aa76a6ef939886ffb909720689dc1f7fc
tobias <tseilertsen@gmail.com>
2020-12-16 21:47:47 +0100

    tign

--- egen.py
+++ egen.py
@@ -87,6 +87,8 @@ def theaterChaseRainbow(strip, wait_ms=10):
 def get_brightness(ttl):
     yellow_val = 10
     color = 250-int(ttl*(250/TOTAL_TTL))
+    if ttl == 0:
+        color = 0
     
     return Color(max(color, 0), max(color, 0), max(int(color/yellow_val), 0), color)
 
