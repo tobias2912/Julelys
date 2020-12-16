103d0f3c498804fd9e4eedb67b8c88eab00da5fa
tobias <tseilertsen@gmail.com>
2020-12-16 19:59:17 +0100

    tign

--- egen.py
+++ egen.py
@@ -89,6 +89,8 @@ def get_brightness(ttl):
     assert color >=0
     if ttl == 0:
         color = 0
+    if color != 0:
+        print(color)
     return Color(color, color, color, color)
 
 def julelys(strip, lightvalues):
